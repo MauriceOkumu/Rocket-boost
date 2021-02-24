ebad99729989188a3aa31bc1d4cc6741177a1d32
maurice <maurice.okumu@thoughtworks.com>
2021-02-24 09:40:50 -0600

    Created the first obstacle and bare bones rocket flying

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField]
+    float thrust = 100f, rotationThrust = 0.5f;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+       rb = GetComponent<Rigidbody>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    void ProcessThrust()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            rb.AddRelativeForce(Vector3.up * thrust );
+        }
+    }
+
+    void ProcessRotation()
+    {
+         if(Input.GetKey(KeyCode.LeftArrow))
+        {
+             ApplyRotation(-rotationThrust);
+        }
+         else if(Input.GetKey(KeyCode.RightArrow))
+        {
+            ApplyRotation(rotationThrust);
+        }
+
+    }
+    void ApplyRotation(float rotateThis)
+    {
+        rb.freezeRotation = true;
+        transform.Rotate(Vector3.back * rotateThis );
+        rb.freezeRotation = false;
+    }
+}
+           
